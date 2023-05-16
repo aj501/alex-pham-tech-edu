7cac87badc4e7b3394056d35f92fad54345984ac
Alex Pham <alex.j.pham@gmail.com>
2023-05-16 12:46:23 -0500

    add homepage and background image

--- engineers-talk/src/app/core/homepage/homepage.component.ts
+++ engineers-talk/src/app/core/homepage/homepage.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styles: [
-  ]
+  styleUrls: ['./homepage.component.css']
+  
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  @ViewChild('typingSound')
+  typingSound!: ElementRef<HTMLAudioElement>;
+  animate = false;
+  typedText = '';
+  text = 'Hi! This is Alex Pham. Thank you for visiting my page.';
+  interval = 100; // Delay between each letter typing animation (in milliseconds)
+
+
+  ngOnInit() {
+    this.animateTyping();
+  }
+  animateTyping() {
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i === this.text.length) {
+        clearInterval(typingInterval);
+        this.animate = true;
+        this.playTypingSound();
+      } else {
+        this.typedText = this.text.slice(0, i + 1);
+        i++;
+      }
+    }, this.interval);
+  }
+
+  playTypingSound() {
+    const audioElement: HTMLAudioElement = this.typingSound.nativeElement;
+    audioElement.currentTime = 0;
+    audioElement.play();
+  }
 
 }
